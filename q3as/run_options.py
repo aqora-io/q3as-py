0a98402cc062dff9eb12a6f509030879c6580944
Julian Popescu <hi@julian.dev>
2024-09-26 13:27:59 +0200

    feat: Add backend to run_options

--- q3as/run_options.py
+++ q3as/run_options.py
@@ -1,3 +1,4 @@
+from typing import Literal
 from pydantic import BaseModel
 
 
@@ -10,5 +11,6 @@ class EstimatorOptions(BaseModel):
 
 
 class RunOptions(BaseModel):
+    backend: Literal["cpu_aer"] = "cpu_aer"
     sampler: SamplerOptions = SamplerOptions()
     estimator: EstimatorOptions = EstimatorOptions()
