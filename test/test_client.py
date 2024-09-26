9ee5363a8087ce3d7c3fd136808529668c17416b
Julian Popescu <hi@julian.dev>
2024-09-26 18:49:08 +0200

    test: Add test for scaleway

--- test/test_client.py
+++ test/test_client.py
@@ -5,6 +5,7 @@ from qiskit.circuit.library import EfficientSU2
 from qiskit.quantum_info import SparsePauliOp
 
 from q3as.algo.vqe import VQEResult
+from q3as.run_options import RunOptions, EstimatorOptions, SamplerOptions
 from q3as import Client, Credentials, VQE
 
 url = os.getenv("Q3AS_URL", "http://localhost:8080")
@@ -33,6 +34,28 @@ async def test_client_no_init_params():
     ansatz = ansatz = EfficientSU2(hamiltonian.num_qubits)
     with Client(Credentials.load(".credentials.json"), url=url) as client:
         _ = VQE.builder().ansatz(ansatz).observables(hamiltonian).send(client)
+
+
+async def test_client_scaleway():
+    hamiltonian = SparsePauliOp.from_list(
+        [("YZ", 0.3980), ("ZI", -0.3980), ("ZZ", -0.0113), ("XX", 0.1810)]
+    )
+    ansatz = ansatz = EfficientSU2(hamiltonian.num_qubits)
+    with Client(Credentials.load(".credentials.json"), url=url) as client:
+        _ = (
+            VQE.builder()
+            .ansatz(ansatz)
+            .observables(hamiltonian)
+            .maxiter(10)
+            .send(
+                client,
+                RunOptions(
+                    backend="scaleway",
+                    estimator=EstimatorOptions(shots=100),
+                    sampler=SamplerOptions(shots=100),
+                ),
+            )
+        )
 
 
 async def test_client_result():
