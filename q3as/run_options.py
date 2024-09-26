50e45e9dab4d59c2085b8a476ec2211a6ec51f6e
Julian Popescu <hi@julian.dev>
2024-09-26 17:05:31 +0200

    feat: Add all backend options

--- q3as/run_options.py
+++ q3as/run_options.py
@@ -11,6 +11,19 @@ class EstimatorOptions(BaseModel):
 
 
 class RunOptions(BaseModel):
-    backend: Literal["cpu_aer"] = "cpu_aer"
+    backend: Literal[
+        "q3as",
+        "q3as:aer_cpu",
+        "scaleway",
+        "scaleway:aer_simulation_2l4",
+        "scaleway:aer_simulation_2l40s",
+        "scaleway:aer_simulation_4l40s",
+        "scaleway:aer_simulation_8l40s",
+        "scaleway:aer_simulation_h100",
+        "scaleway:aer_simulation_2h100",
+        "scaleway:aer_simulation_pop_c16m128",
+        "scaleway:aer_simulation_pop_c32m256",
+        "scaleway:aer_simulation_pop_c64m512",
+    ] = "q3as"
     sampler: SamplerOptions = SamplerOptions()
     estimator: EstimatorOptions = EstimatorOptions()
